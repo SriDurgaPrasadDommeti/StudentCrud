18c3c4cc8d94fd445db588395babf5e2e886e74d
SriDurgaPrasadDommeti <sridurgaprasaddommeti@gmail.com>
2026-09-24 17:52:17 +0530

    added

--- Student_management_system/src/main/java/com/example/demo/Controllers/StudentController.java
+++ Student_management_system/src/main/java/com/example/demo/Controllers/StudentController.java
@@ -26,7 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class StudentController {
 	private final StudentService studentService;
-
+	//create
     @PostMapping("/create")
     public ResponseEntity<StudentResponseDTO> createStudent(@Valid @RequestBody StudentRequestDTO dto) {
         return new ResponseEntity<>(studentService.createStudent(dto), HttpStatus.CREATED);
